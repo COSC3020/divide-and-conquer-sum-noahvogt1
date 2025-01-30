31ee205ced2c08d58c198f6793a0e8004f50cc8f
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-01-30 13:18:13 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 function divideAndConquerSum(a) {
-    if (a.length === 0) return 0;
-    if (a.length === 1) return a[0];
-    if (a.length === 2) return a[0] + a[1];
+    if (a.length == 0) return 0;
+    if (a.length == 1) return a[0];
     
-    third = Math.ceil(a.length / 3);
-    left = divideAndConquerSum(a.slice(0, third));
-    middle = divideAndConquerSum(a.slice(third, third * 2));
-    right = divideAndConquerSum(a.slice(third * 2));
+    var third = Math.floor(a.length / 3);
+    var twoThirds = Math.floor((2 * a.length) / 3)
+    var left = divideAndConquerSum(a.slice(0, third));
+    var middle = divideAndConquerSum(a.slice(third, twoThirds));
+    var right = divideAndConquerSum(a.slice(twoThirds));
     return left + middle + right;
 }
